f10b3773ed1cf0a31deac71a24508bfd82527dc6
JHavlik97 <103044841+JHavlik97@users.noreply.github.com>
2022-04-05 09:19:58 -0400

    Update Case_9_Data.R

--- Case_9_Data.R
+++ Case_9_Data.R
@@ -5,3 +5,6 @@ View(Case9Data)
 
 #binning WebUse column
 Case9Data$WebUseBin <- ifelse(Case9Data$WebUse %in% NA, 0, ifelse(Case9Data$WebUse == 0, 0, 1))
+Case9Data$canordersBin <- ifelse(Case9Data$canorders == 0, 0, 1)
+Case9Data$PrdoCatD_Bin <- ifelse(Case9Data$ProdCatD %in% NA, 0, ifelse(Case9Data$ProdCatD<=100,1, 2))
+Case9Data$ProdCatE_Bin <- ifelse(Case9Data$ProdCatE %in% NA, 0, ifelse(Case9Data$ProdCatE<=100,1, 2))
