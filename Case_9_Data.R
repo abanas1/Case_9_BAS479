3bb330fa7f8db7b39b0bdd02ac1c3b183d72704c
Brayden Campbell <103044887+bcampb47@users.noreply.github.com>
2022-04-14 17:17:06 -0400

    Fix Var Name

--- Case_9_Data.R
+++ Case_9_Data.R
@@ -8,7 +8,7 @@ Case9Data$WebUseBin <- ifelse(Case9Data$WebUse %in% NA, 0, ifelse(Case9Data$WebU
 Case9Data$canordersBin <- ifelse(Case9Data$canorders == 0, 0, 1)
 Case9Data$ProdCatB_Bin <- ifelse(Case9Data$ProdCatB %in% NA, 0, ifelse(Case9Data$ProdCatB <= 100, 1, 2))
 Case9Data$ProdCatC_Bin <- ifelse(Case9Data$ProdCatC %in% NA, 0, ifelse(Case9Data$ProdCatC<=100,1,2))
-Case9Data$PrdoCatD_Bin <- ifelse(Case9Data$ProdCatD %in% NA, 0, ifelse(Case9Data$ProdCatD<=100,1, 2))
+Case9Data$ProdCatD_Bin <- ifelse(Case9Data$ProdCatD %in% NA, 0, ifelse(Case9Data$ProdCatD<=100,1, 2))
 Case9Data$ProdCatE_Bin <- ifelse(Case9Data$ProdCatE %in% NA, 0, ifelse(Case9Data$ProdCatE<=100,1, 2))
 #Exporting CSV File
 path <- "C:\\Users\\jakob\\Documents\\GitHub\\Case_9_BAS479" #When Using, Set file path to your folder
