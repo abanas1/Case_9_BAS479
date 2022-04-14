f1e7149c48874a53a2784861987d6a10eb2a10f1
JHavlik97 <103044841+JHavlik97@users.noreply.github.com>
2022-04-14 15:56:34 -0400

    Update Case_9_Data.R

--- Case_9_Data.R
+++ Case_9_Data.R
@@ -10,3 +10,6 @@ Case9Data$ProdCatB_Bin <- ifelse(Case9Data$ProdCatB %in% NA, 0, ifelse(Case9Data
 Case9Data$ProdCatC_Bin <- ifelse(Case9Data$ProdCatC %in% NA, 0, ifelse(Case9Data$ProdCatC<=100,1,2))
 Case9Data$PrdoCatD_Bin <- ifelse(Case9Data$ProdCatD %in% NA, 0, ifelse(Case9Data$ProdCatD<=100,1, 2))
 Case9Data$ProdCatE_Bin <- ifelse(Case9Data$ProdCatE %in% NA, 0, ifelse(Case9Data$ProdCatE<=100,1, 2))
+#Exporting CSV File
+path <- "C:\\Users\\jakob\\Documents\\GitHub\\Case_9_BAS479" #When Using, Set file path to your folder
+write.csv(Case9Data, file.path(path, "Case9DataBinned.csv"), row.names = FALSE)
